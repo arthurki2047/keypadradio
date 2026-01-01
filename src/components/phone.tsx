b5aacd4660c5db154d8e87115c16a8a3e237971e
Arthur <arthurki2047@gmail.com>
2026-01-01 14:07:04 +0000

    remove keypad from screen

--- src/components/phone.tsx
+++ src/components/phone.tsx
@@ -1,20 +1,18 @@
 "use client";
 
 import { Screen } from './screen';
-import { Keypad } from './keypad';
 import { useKeypad } from '@/hooks/use-keypad';
 
 type PhoneProps = ReturnType<typeof useKeypad>;
 
 export function Phone(props: PhoneProps) {
     return (
-        <div className="relative mx-auto h-[520px] w-[260px] rounded-[30px] border-[10px] border-neutral-800 bg-neutral-800 shadow-2xl dark:border-neutral-900 dark:bg-neutral-900">
+        <div className="relative mx-auto h-[480px] w-[280px] rounded-[30px] border-[10px] border-neutral-800 bg-neutral-800 shadow-2xl dark:border-neutral-900 dark:bg-neutral-900">
             <div className="h-full w-full overflow-hidden rounded-[20px] bg-background flex flex-col">
                 <div className="w-full h-6 bg-neutral-800 dark:bg-neutral-900 flex justify-center items-end pb-1">
                     <div className="w-16 h-1.5 bg-neutral-700 rounded-full"></div>
                 </div>
                 <Screen {...props} />
-                <Keypad onKeyPress={props.handleKeyPress} />
             </div>
         </div>
     );
--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -29,7 +29,7 @@ export function Screen(props: ScreenProps) {
   };
 
   return (
-    <div className="h-[280px] w-[240px] bg-background p-2 overflow-hidden relative">
+    <div className="flex-1 w-full bg-background p-2 overflow-hidden relative">
       <AnimatePresence mode="wait">
         <motion.div
           key={props.view}
