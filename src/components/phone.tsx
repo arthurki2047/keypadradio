54db4c0379ce4faed01b568f13448a667b2f96a0
Arthur <arthurki2047@gmail.com>
2026-01-01 14:05:40 +0000

    reduce screen keypad size

--- src/components/phone.tsx
+++ src/components/phone.tsx
@@ -8,7 +8,7 @@ type PhoneProps = ReturnType<typeof useKeypad>;
 
 export function Phone(props: PhoneProps) {
     return (
-        <div className="relative mx-auto h-[580px] w-[280px] rounded-[30px] border-[10px] border-neutral-800 bg-neutral-800 shadow-2xl dark:border-neutral-900 dark:bg-neutral-900">
+        <div className="relative mx-auto h-[520px] w-[260px] rounded-[30px] border-[10px] border-neutral-800 bg-neutral-800 shadow-2xl dark:border-neutral-900 dark:bg-neutral-900">
             <div className="h-full w-full overflow-hidden rounded-[20px] bg-background flex flex-col">
                 <div className="w-full h-6 bg-neutral-800 dark:bg-neutral-900 flex justify-center items-end pb-1">
                     <div className="w-16 h-1.5 bg-neutral-700 rounded-full"></div>
