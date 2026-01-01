907cbb030434838b45c327c8317a8269a07bbb1b
Arthur <arthurki2047@gmail.com>
2026-01-01 15:55:38 +0000

    add  Akashvani Kolkata Sanchayita https://airhlspush.pc.cdn.bitgravity.c

--- src/lib/stations.ts
+++ src/lib/stations.ts
@@ -4,8 +4,9 @@ export const stations: Station[] = [
   { id: 's9', name: 'AIR Kolkata', streamUrl: 'http://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/kolkata/128/128' },
   { id: 's10', name: 'Vividh Bharati', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/bharati/128/128' },
   { id: 's11', name: 'AIR FM Gold (Kolkata)', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio057/hlspbaudio05764kbps.m3u8', logoUrl: 'https://picsum.photos/seed/gold/128/128' },
-  { id: 's12', name: 'AIR Murshidabad', streamUrl: 'http://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/murshidabad/128/128' },
+  { id: 's12', name: 'AIR Murshidabad', streamUrl: 'http://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/murshidabad-new/128/128' },
   { id: 's13', name: 'Air Rainbow 107 Kolkata', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio058/hlspbaudio05864kbps.m3u8', logoUrl: 'https://picsum.photos/seed/rainbow/128/128' },
   { id: 's14', name: 'Kolkata - Geetanjali', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio055/hlspbaudio05564kbps.m3u8', logoUrl: 'https://picsum.photos/seed/geetanjali/128/128' },
   { id: 's15', name: 'All India Radio News 24X7', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio002/hlspbaudio002_Auto.m3u8', logoUrl: 'https://picsum.photos/seed/news/128/128' },
+  { id: 's16', name: 'Akashvani Kolkata Sanchayita', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio056/hlspbaudio05664kbps.m3u8', logoUrl: 'https://picsum.photos/seed/sanchayita/128/128' },
 ];
