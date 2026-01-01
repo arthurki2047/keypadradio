3f50a865101446913dc8ca74d70f8ead0cf4d74c
Arthur <arthurki2047@gmail.com>
2026-01-01 16:03:16 +0000

    make also touchable format like ok , back , etc . do not overwrite anyth

--- src/components/phone.tsx
+++ src/components/phone.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Screen } from './screen';
+import { Keypad } from './keypad';
 import { useKeypad } from '@/hooks/use-keypad';
 import { Clock } from './clock';
 
@@ -8,13 +9,16 @@ type PhoneProps = ReturnType<typeof useKeypad>;
 
 export function Phone(props: PhoneProps) {
     return (
-        <div className="relative mx-auto h-[480px] w-[280px] rounded-[30px] border-[10px] border-neutral-800 bg-neutral-800 shadow-2xl dark:border-neutral-900 dark:bg-neutral-900">
+        <div className="relative mx-auto h-[480px] w-[280px] rounded-[30px] border-[10px] border-neutral-800 bg-neutral-800 shadow-2xl dark:border-neutral-900 dark:bg-neutral-900 flex flex-col">
             <div className="h-full w-full overflow-hidden rounded-[20px] bg-background flex flex-col">
                 <div className="w-full h-8 bg-neutral-800 dark:bg-neutral-900 flex justify-between items-center px-4">
                     <div className="w-16 h-1.5 bg-neutral-700 rounded-full"></div>
                     <Clock />
                 </div>
                 <Screen {...props} />
+            </div>
+            <div className="absolute bottom-0 left-0 right-0">
+                <Keypad onKeyPress={props.handleKeyPress} />
             </div>
         </div>
     );
