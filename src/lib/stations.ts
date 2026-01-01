15cc616c7b00f1b32231e9255b9674df4a3da149
Arthur <arthurki2047@gmail.com>
2026-01-01 13:32:11 +0000

    Add AIR FM Gold (Kolkata) https://airhlspush.pc.cdn.bitgravity.com/httpp

--- src/lib/stations.ts
+++ src/lib/stations.ts
@@ -8,4 +8,5 @@ const getImageUrl = (id: string) => imageUrls.find((i) => i.id === id)?.imageUrl
 export const stations: Station[] = [
   { id: 's9', name: 'AIR Kolkata', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio055/hlspbaudio05564kbps.m3u8', logoUrl: getImageUrl('radio-9') },
   { id: 's10', name: 'Vividh Bharati', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8', logoUrl: getImageUrl('radio-10') },
+  { id: 's11', name: 'AIR FM Gold (Kolkata)', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio057/hlspbaudio057_Auto.m3u8', logoUrl: getImageUrl('radio-11') },
 ];
