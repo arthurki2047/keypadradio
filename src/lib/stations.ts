91c849fc781375de33658b9293cb022cefeebc63
Arthur <arthurki2047@gmail.com>
2026-01-01 13:39:25 +0000

    add AIR Murshidabad https://air.pc.cdn.bitgravity.com/air/live/pbaudio16

--- src/lib/stations.ts
+++ src/lib/stations.ts
@@ -9,4 +9,5 @@ export const stations: Station[] = [
   { id: 's9', name: 'AIR Kolkata', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio055/hlspbaudio05564kbps.m3u8', logoUrl: getImageUrl('radio-9') },
   { id: 's10', name: 'Vividh Bharati', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8', logoUrl: getImageUrl('radio-10') },
   { id: 's11', name: 'AIR FM Gold (Kolkata)', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio057/hlspbaudio057_Auto.m3u8', logoUrl: getImageUrl('radio-11') },
+  { id: 's12', name: 'AIR Murshidabad', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: getImageUrl('radio-12') },
 ];
