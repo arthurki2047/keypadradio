2be4a8d9af0a2dac6b575317869e8d18eb855041
Arthur <arthurki2047@gmail.com>
2026-01-01 15:41:28 +0000

    AIR Murshidabad

--- src/lib/stations.ts
+++ src/lib/stations.ts
@@ -4,7 +4,7 @@ export const stations: Station[] = [
   { id: 's9', name: 'AIR Kolkata', streamUrl: 'http://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/radio-9/100/100' },
   { id: 's10', name: 'Vividh Bharati', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/radio-10/100/100' },
   { id: 's11', name: 'AIR FM Gold (Kolkata)', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio057/hlspbaudio05764kbps.m3u8', logoUrl: 'https://picsum.photos/seed/radio-11/100/100' },
-  { id: 's12', name: 'AIR Murshidabad', streamUrl: 'http://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/radio-12/100/100' },
+  { id: 's12', name: 'AIR Murshidabad', streamUrl: 'http://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/murshidabad-new/100/100' },
   { id: 's13', name: 'Air Rainbow 107 Kolkata', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio058/hlspbaudio05864kbps.m3u8', logoUrl: 'https://picsum.photos/seed/radio-13/100/100' },
   { id: 's14', name: 'Kolkata - Geetanjali', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio055/hlspbaudio05564kbps.m3u8', logoUrl: 'https://picsum.photos/seed/radio-14/100/100' },
 ];
