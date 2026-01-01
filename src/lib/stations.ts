25ee43957236e0e5d43d3a52e110ea23ec25c156
Arthur <arthurki2047@gmail.com>
2026-01-01 15:33:27 +0000

    AIR FM Gold Kolkata url updatehttps://airhlspush.pc.cdn.bitgravity.com/h

--- src/lib/stations.ts
+++ src/lib/stations.ts
@@ -8,7 +8,7 @@ const getImageUrl = (id: string) => imageUrls.find((i) => i.id === id)?.imageUrl
 export const stations: Station[] = [
   { id: 's9', name: 'AIR Kolkata', streamUrl: 'http://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: getImageUrl('radio-9') },
   { id: 's10', name: 'Vividh Bharati', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8', logoUrl: getImageUrl('radio-10') },
-  { id: 's11', name: 'AIR FM Gold (Kolkata)', streamUrl: 'http://103.120.125.13:8000/live', logoUrl: getImageUrl('radio-11') },
+  { id: 's11', name: 'AIR FM Gold (Kolkata)', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio057/hlspbaudio05764kbps.m3u8', logoUrl: getImageUrl('radio-11') },
   { id: 's12', name: 'AIR Murshidabad', streamUrl: 'http://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: getImageUrl('radio-12') },
   { id: 's13', name: 'Air Rainbow 107 Kolkata', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio058/hlspbaudio05864kbps.m3u8', logoUrl: getImageUrl('radio-13') },
   { id: 's14', name: 'Kolkata - Geetanjali', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio055/hlspbaudio05564kbps.m3u8', logoUrl: getImageUrl('radio-14') },
