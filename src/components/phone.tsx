30c75b759bd792b8a05dbc7a9c30fd9a6aac0771
Arthur <arthurki2047@gmail.com>
2026-08-08 16:48:42 +0000

    When screen timeout will occur it will automatically actually physically

--- src/components/phone.tsx
+++ src/components/phone.tsx
@@ -3,31 +3,53 @@
 
 import { Screen } from './screen';
 import { useKeypad } from '@/hooks/use-keypad';
-import { Power } from 'lucide-react';
 import { Keypad } from './keypad';
+import { cn } from '@/lib/utils';
 
 type PhoneProps = ReturnType<typeof useKeypad>;
 
 export function Phone(props: PhoneProps) {
-    return (
-        <div className="relative mx-auto h-[480px] w-[240px] rounded-[30px] border-[8px] border-neutral-800 bg-neutral-800 shadow-2xl dark:border-neutral-900 dark:bg-neutral-900 flex flex-col">
-            <div className="h-full w-full overflow-hidden rounded-[22px] bg-background flex flex-col">
-                <div className="w-full h-6 bg-neutral-800 dark:bg-neutral-900 flex justify-center items-center px-3">
-                    <div className="w-12 h-1 bg-neutral-700 rounded-full"></div>
-                </div>
-                <div className="flex-1 w-full bg-background overflow-hidden relative">
-                    {props.isScreenOn ? (
-                        <Screen {...props} />
-                    ) : (
-                        <div className="w-full h-full bg-black flex flex-col items-center justify-center text-neutral-500">
-                            <Power className="w-12 h-12 mb-2" />
-                            <p className="text-xs">Screen is off</p>
-                            <p className="text-[10px] mt-1">Press any key to wake</p>
-                        </div>
-                    )}
-                </div>
-                {/* <Keypad onKeyPress={props.handleKeyPress} /> */}
-            </div>
+  const { isScreenOn, handleKeyPress } = props;
+
+  return (
+    <div className="relative mx-auto w-[280px] h-[580px] rounded-[40px] border-[10px] border-neutral-900 bg-neutral-900 shadow-2xl flex flex-col overflow-hidden select-none">
+      {/* Top Earpiece / Sensor Area */}
+      <div className="w-full h-8 flex justify-center items-center bg-neutral-900">
+        <div className="w-14 h-1.5 bg-neutral-800 rounded-full shadow-inner" />
+      </div>
+
+      {/* Main Display Area */}
+      <div className="flex-1 relative w-full overflow-hidden bg-black">
+        {/* The active interface - hidden completely when off */}
+        <div className={cn(
+          "w-full h-full transition-all duration-700 ease-in-out",
+          !isScreenOn && "brightness-0 opacity-0 pointer-events-none"
+        )}>
+          <Screen {...props} />
         </div>
-    );
+
+        {/* The "Off" screen overlay - simulates physical blackout */}
+        {!isScreenOn && (
+          <div 
+            className="absolute inset-0 z-50 bg-black cursor-pointer active:bg-neutral-950 transition-colors"
+            onClick={() => handleKeyPress('Wake')}
+            title="Press any key to wake"
+          >
+            {/* Deep black void simulating a powered-off LCD */}
+          </div>
+        )}
+      </div>
+
+      {/* Integrated Keypad Section */}
+      <div className={cn(
+        "w-full bg-neutral-900 p-2 pb-6 transition-all duration-700 ease-in-out",
+        !isScreenOn && "opacity-30 grayscale contrast-75 brightness-50 pointer-events-auto"
+      )}>
+        <Keypad onKeyPress={handleKeyPress} />
+      </div>
+
+      {/* Bottom Decorative Element */}
+      <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-1.5 h-1.5 bg-neutral-800 rounded-full opacity-50" />
+    </div>
+  );
 }
