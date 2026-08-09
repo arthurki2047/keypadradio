df962548d80b5e8a8cc0a79677fdcc1973b92d78
Arthur <arthurki2047@gmail.com>
2026-08-09 10:59:31 +0000

    update stream urls Geet Radio 24/7 Bangla Music https://server.geetradio

--- src/lib/stations.ts
+++ src/lib/stations.ts
@@ -2,17 +2,17 @@
 import type { Station } from '@/types';
 
 export const stations: Station[] = [
-  { id: 's9', name: 'AIR Kolkata', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/kolkata/128/128' },
-  { id: 's10', name: 'Vividh Bharati', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/bharati/128/128' },
+  { id: 's9', name: 'AIR Kolkata', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio275/hlspbaudio275_Auto.m3u8', logoUrl: 'https://picsum.photos/seed/kolkata/128/128' },
+  { id: 's10', name: 'Vividh Bharati', streamUrl: 'https://d3hrxqn1tritdh.cloudfront.net/146ed6ec6dea5a24/146ed6ec6dea5a24.m3u8', logoUrl: 'https://picsum.photos/seed/bharati/128/128' },
   { id: 's11', name: 'AIR FM Gold (Kolkata)', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio057/hlspbaudio05764kbps.m3u8', logoUrl: 'https://picsum.photos/seed/gold/128/128' },
-  { id: 's12', name: 'AIR Murshidabad', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio169/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/murshidabad-new/128/128' },
+  { id: 's12', name: 'AIR Murshidabad', streamUrl: 'https://d1cvqgmbcpg5yn.cloudfront.net/47a45f818dd9203b/47a45f818dd9203b.m3u8', logoUrl: 'https://picsum.photos/seed/murshidabad-new/128/128' },
   { id: 's13', name: 'Air Rainbow 107 Kolkata', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio058/hlspbaudio05864kbps.m3u8', logoUrl: 'https://picsum.photos/seed/rainbow/128/128' },
   { id: 's14', name: 'Kolkata - Geetanjali', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio055/hlspbaudio05564kbps.m3u8', logoUrl: 'https://picsum.photos/seed/geetanjali/128/128' },
   { id: 's15', name: 'All India Radio News 24X7', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio192/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/news/128/128' },
   { id: 's16', name: 'Akashvani Kolkata Sanchayita', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio056/hlspbaudio05664kbps.m3u8', logoUrl: 'https://picsum.photos/seed/sanchayita/128/128' },
   { id: 's17', name: 'Akashvani Maitree', streamUrl: 'https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio245/hlspbaudio24564kbps.m3u8', logoUrl: 'https://picsum.photos/seed/maitree/128/128' },
-  { id: 's18', name: 'Akashvani Shantiniketan', streamUrl: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio247/playlist.m3u8', logoUrl: 'https://picsum.photos/seed/shantiniketan/128/128' },
-  { id: 's19', name: 'Radio Mirchi', streamUrl: 'https://19013.live.streamtheworld.com/NJS_HIN_EST_SC', logoUrl: 'https://picsum.photos/seed/mirchi/128/128' },
+  { id: 's18', name: 'Akashvani Shantiniketan', streamUrl: 'https://d3hrxqn1tritdh.cloudfront.net/66249dfecaf80241/66249dfecaf80241.m3u8', logoUrl: 'https://picsum.photos/seed/shantiniketan/128/128' },
+  { id: 's19', name: 'Radio Mirchi', streamUrl: 'https://eu8.fastcast4u.com/proxy/clyedupq?mp=%2F1?aw_0_req_lsid=2c0fae177108c9a42a7cf24878625444', logoUrl: 'https://picsum.photos/seed/mirchi/128/128' },
   { id: 's20', name: '92.7 Big FM', streamUrl: 'https://stream-151.zeno.fm/dbstwo3dvhhtv', logoUrl: 'https://picsum.photos/seed/bigfm/128/128' },
   { id: 's21', name: 'Red FM', streamUrl: 'https://stream-175.zeno.fm/q97eczydqrhvv', logoUrl: 'https://picsum.photos/seed/redfm/128/128' },
   { id: 's22', name: 'Bollywood Dance', streamUrl: 'https://3.mystreaming.net/uber/bollywooddance/icecast.audio', logoUrl: 'https://picsum.photos/seed/bollywood-dance/128/128' },
@@ -21,5 +21,5 @@ export const stations: Station[] = [
   { id: 's25', name: 'Bollywood 2000s', streamUrl: 'https://nl4.mystreaming.net/uber/bollywood2000s/icecast.audio', logoUrl: 'https://picsum.photos/seed/bollywood-2000s/128/128' },
   { id: 's26', name: 'My Club Remix', streamUrl: 'https://stream-160.zeno.fm/pkvh8zhyzp8uv', logoUrl: 'https://picsum.photos/seed/club-remix/128/128' },
   { id: 's27', name: 'Mixify Bengali Hits', streamUrl: 'https://server.mixify.in/listen/bangla/radio.mp3', logoUrl: 'https://picsum.photos/seed/mixify-bengali/128/128' },
-  { id: 's28', name: 'Geet Radio 24/7 Bangla Music', streamUrl: 'https://server.geetradio.com:8050/radio.mp3', logoUrl: 'https://picsum.photos/seed/geet-radio/128/128' },
+  { id: 's28', name: 'Geet Radio 24/7 Bangla Music', streamUrl: 'https://server.geetradio.com/listen/bangla/radio.mp3', logoUrl: 'https://picsum.photos/seed/geet-radio/128/128' },
 ];
